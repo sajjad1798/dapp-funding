947324bf3e254eec9607cb99e0f1222ac8a90562
sajjad <muhammadsajjad1798@gmail.com>
2022-12-06 16:39:30 +0500

    add static ui

--- funding/src/App.js
+++ funding/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="text-center bg-light py-2 border-bottom">
+        <h3>Funding App</h3>
       </header>
+      <section className='py-3 d-flex flex-column align-items-center'>
+        <h4>Balance: 20ETH</h4>
+        <span>Account: 0x000000000</span>
+        <div className="mt-4 d-flex gap-3">
+          <button className="btn btn-primary">Connect to metaMask</button>
+          <button className="btn btn-success">Transfer</button>
+          <button className="btn btn-success">Withdraw</button>
+        </div>        
+      </section>
+      <footer className="py-2 bg-light text-center border-top ">
+        Copyright 
+      </footer>
     </div>
   );
 }
